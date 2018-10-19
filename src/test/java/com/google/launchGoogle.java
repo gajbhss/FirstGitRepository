14755e8740801653b861fcc78cd3c7588385e9a2
sanket gajbhiv <sanketgajbhiv@Raj-Mac-mini.local>
2018-10-19 11:51:05 +0530

    first commit

---
+++ src/test/java/com/google/launchGoogle.java
@@ -0,0 +1,23 @@
+package com.google;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class launchGoogle {
+	
+	WebDriver driver;
+	
+	@Test
+	public void openbrowser()
+	{
+		System.setProperty("webdriver.chrome.driver", "/Users/sanketgajbhiv/Downloads/Sanket_Doc/Sanket_Joining_Documents/Scanned Copy/chromedriver");
+		driver=new ChromeDriver();
+		driver.get("https://www.google.com");
+		System.out.println("End");
+		driver.quit();
+		
+	}
+
+}
